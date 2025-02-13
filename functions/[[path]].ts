5345961a4f6653069a2217d86fe29540b8d02f7e
Zach Kelling <z@zeekay.io>
2025-02-13 01:11:35 -0600

    Force nodejs runtime

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,3 +1,5 @@
+export const config = { runtime: 'nodejs' };
+
 import { createRequestHandler } from '@remix-run/vercel';
 // @ts-ignore: Build output is generated at runtime.
 import * as build from '../build/server/index.js';
